d04d92acb5f5861c59cad7379e47bd9c1fe8df61
Dmytro Lizunov <dmytro_lizunov@epam.com>
2021-02-17 20:17:08 +0200

    Init

--- src/main/kotlin/com/epam/classes/data/Fuel.kt
+++ src/main/kotlin/com/epam/classes/data/Fuel.kt
@@ -1,6 +1,7 @@
 package com.epam.classes.data
 
 sealed class Fuel(val costPerLiter: Int = 0, val discountValue: Int = 0)
+
 /**
  * All these classes should be inherited from com.epam.classes.data.Fuel class
  */
